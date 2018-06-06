fbb149e7c4f78618d8c9a7ce84d02f9b4122f892
Jared Eklin <jaredeklin@gmail.com>
2018-06-05 21:14:02 -0600

    Update tests to EventForm tests to include clean date/time method

--- src/EventForm/EventForm.js
+++ src/EventForm/EventForm.js
@@ -46,9 +46,7 @@ export class EventForm extends Component {
 
     const dateTime = this.api.cleanDateTime(this.state.date, this.state.time)
     const { date, time } = dateTime 
-    console.log(date)
-    console.log(time)
-    
+
     const eventObj = {
       venue,
       name,
