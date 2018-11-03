5d20e889757797d1dc3603c3fa46155c330f4b7d
Jared Eklin <jaredeklin@gmail.com>
2018-11-03 12:49:09 -0600

    Update tab tests to improve coverage

--- src/Tab/Tab.test.js
+++ src/Tab/Tab.test.js
@@ -3,8 +3,36 @@ import { shallow } from 'enzyme';
 import { Tab } from './Tab';
 
 describe('Tab', () => {
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<Tab />);
+  let wrapper;
+  const mockHandleTabClick = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Tab
+        handleTabClick={mockHandleTabClick}
+        isActive={true}
+        tabName="Schedule"
+      />
+    );
+  });
+
+  it('should match the snapshot when isActive is true', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot when isActive is false', () => {
+    wrapper = shallow(
+      <Tab
+        handleTabClick={mockHandleTabClick}
+        isActive={false}
+        tabName="Schedule"
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call handleTabClick with correct params when clicked', () => {
+    wrapper.find('li').simulate('click');
+    expect(mockHandleTabClick).toHaveBeenCalledWith('Schedule');
   });
 });
