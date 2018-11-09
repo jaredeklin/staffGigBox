4d74b05df2202932f36f792c01045f98baf7b37e
Jared Eklin <jaredeklin@gmail.com>
2018-11-09 15:17:33 -0700

    Create shuffleStaffArray to randomize scheduling of staff

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -431,4 +431,12 @@ export class Api {
 
     return this.availableStaff;
   };
+  shuffleStaffArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+  };
 }
