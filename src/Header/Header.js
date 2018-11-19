172bb7150c1a2cc0a4bca3c9ba63706f95c5265a
Jared Eklin <jaredeklin@gmail.com>
2018-11-19 13:01:53 -0700

    Create Header component

---
+++ src/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import ScheduleNavBar from '../ScheduleNavBar';
+
+const Header = ({ location }) => {
+  const getHeaderText = () => {
+    switch (location.pathname) {
+      case '/availability':
+        return 'Availability';
+
+      case '/add-events':
+        return 'Add Events';
+
+      case '/add-staff':
+        return 'Add Staff';
+
+      case '/unscheduled-events':
+        return 'Unscheduled Events';
+
+      default:
+        return 'Schedule';
+    }
+  };
+
+  return (
+    <header className="main-header">
+      <h1>{getHeaderText()}</h1>
+      {location.pathname.includes('/schedule') && <ScheduleNavBar />}
+    </header>
+  );
+};
+
+export default withRouter(Header);
